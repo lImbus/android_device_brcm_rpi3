ee383915ffdde4455afa7b4faf9a7c1e90708dd6
Mimoja <schander@embedded.rwth-aachen.de>
2017-01-06 15:45:13 +0100

    Finished Download logic. Patches needed

--- scripts/video/precursor.sh
+++ scripts/video/precursor.sh
@@ -1,25 +1,40 @@
 
 
 
-if [ $# -eq 0 ]
+if [ $# -le 1 ]
   then
-    echo "Please provide the needed patchfile"
-    echo "$0 {$PATCHES}"
+    echo "Please provide the needed patchfile and the sdcard mountpoint"
+    echo "$0 MOUNTPOINT {$PATCHES}"
     exit 1
 fi
 
-PATCHFILE=$1
+MOUNTPOINT=$1
+PATCHFILE=$2
 
 #create download link
-GITHUB_RAW="https://raw.githubusercontent.com/RTAndroid/android_device_brcm_rpi3/$CURRENT_HASH/$PATCH_PATH/\$PATCHFILE"
+GITHUB_RAW="https://raw.githubusercontent.com/RTAndroid/android_device_brcm_rpi3/$CURRENT_HASH/$PATCH_PATH/$PATCHFILE"
 
 PATCH=$(curl -sf $GITHUB_RAW)
+STATUS=$?
+if [ $PATCHFILE="custom" ]
+  then 
+     if [ $# -le 2 ]
+       then
+         echo "Please provide the custom resolution in the form of"
+         echo "$0 MOUNTPOINT custom WIDTHxHEIGHT"
+         exit 1
+     fi
+     RESOLUTION=$3
+     PATCH=${PATCH/$RES_PLACEHOLDER/$RESOLUTION}
+fi
 
-if [ $? -eq 0 ]
+echo $RESOLUTION
+if [ $STATUS -eq 0 ]
   then
     echo "patching filesystem"
-    echo $PATCH | patch
+    echo $PATCH | patch MOUNTPOINT
   else
+    echo "Download failed. Valid patchfile chosen?"
     exit 1
 fi
 
